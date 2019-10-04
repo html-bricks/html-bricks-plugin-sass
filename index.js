61fc8fc4be430037c39c4d575b8d5664667ecc41
Gustav Burchardt <gustavburchardt@gmail.com>
2019-10-04 13:44:39 +0200

    Implement sass plugin

---
+++ index.js
@@ -0,0 +1,23 @@
+var sass = require('node-sass')
+
+const sassReg = /(\.sass|\.scss)$/
+
+exports.postBuild = function (files) {
+  return new Promise((resolve, reject) => {
+    console.log('Compiling sass files\n')
+    const next = files.map(file => {
+      if (file.src.match(sassReg)) {
+        return Object.assign({}, file, {
+          dest: file.dest.replace(sassReg, '.css'),
+          content: sass.renderSync({
+            data: file.content
+          }).css.toString()
+        })
+      } else {
+        return file
+      }
+    })
+
+    resolve(next)
+  })
+}
